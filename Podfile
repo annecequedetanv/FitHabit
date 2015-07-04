6b5e0bc2141064f6a01a6b26e21702748c220762
Natasha Murashev <3365363+annecequedetanv@users.noreply.github.com>
2015-07-04 11:44:17 -0700

    installed pods - Quick, Nimble, and Realm

---
+++ Podfile
@@ -0,0 +1,12 @@
+platform :ios, '9.0'
+inhibit_all_warnings!
+
+use_frameworks!
+
+pod 'Realm',      :git => 'https://github.com/realm/realm-cocoa.git', :branch => 'swift-2.0'
+pod 'RealmSwift', :git => 'https://github.com/realm/realm-cocoa.git', :branch => 'swift-2.0'
+
+link_with 'FitHabitTests', 'FitHabitUITests'
+
+pod 'Quick', '0.4.0'
+pod 'Nimble', '2.0.0-rc.1'
---
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,38 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// Nimble
+#define COCOAPODS_POD_AVAILABLE_Nimble
+#define COCOAPODS_VERSION_MAJOR_Nimble 2
+#define COCOAPODS_VERSION_MINOR_Nimble 0
+#define COCOAPODS_VERSION_PATCH_Nimble 0
+
+// Quick
+#define COCOAPODS_POD_AVAILABLE_Quick
+#define COCOAPODS_VERSION_MAJOR_Quick 0
+#define COCOAPODS_VERSION_MINOR_Quick 4
+#define COCOAPODS_VERSION_PATCH_Quick 0
+
+// Realm
+#define COCOAPODS_POD_AVAILABLE_Realm
+#define COCOAPODS_VERSION_MAJOR_Realm 0
+#define COCOAPODS_VERSION_MINOR_Realm 93
+#define COCOAPODS_VERSION_PATCH_Realm 2
+
+// Realm/Headers
+#define COCOAPODS_POD_AVAILABLE_Realm_Headers
+#define COCOAPODS_VERSION_MAJOR_Realm_Headers 0
+#define COCOAPODS_VERSION_MINOR_Realm_Headers 93
+#define COCOAPODS_VERSION_PATCH_Realm_Headers 2
+
+// RealmSwift
+#define COCOAPODS_POD_AVAILABLE_RealmSwift
+#define COCOAPODS_VERSION_MAJOR_RealmSwift 0
+#define COCOAPODS_VERSION_MINOR_RealmSwift 93
+#define COCOAPODS_VERSION_PATCH_RealmSwift 2
+
